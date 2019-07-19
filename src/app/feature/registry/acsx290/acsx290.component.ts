cabcf7402e83313ee84cec88e9aca159d4f8c9b7
Siriwasan <siriwasan@gmail.com>
2019-07-19 11:54:16 +0700

    clean ACSx290 component file

--- src/app/feature/registry/acsx290/acsx290.component.ts
+++ src/app/feature/registry/acsx290/acsx290.component.ts
@@ -15,13 +15,13 @@ import { validations } from './acsx290.validation';
 import { ACSx290Model } from './acsx290.model';
 import { ACSx290Service } from './acsx290.service';
 import { tableOfContent } from './acsx290.toc';
+import * as acsx290Data from './acsx290.data';
 import { RegistryService } from '../registry.service';
+import { Staff } from '../../staff/staff.model';
 
 import { Store } from '@ngrx/store';
 import * as fromRoot from '../../../app.reducer';
 import * as UI from '../../../shared/ui.actions';
-
-import { staffs, Staff } from '../../staff/staff.model';
 
 @Component({
   selector: 'app-acsx290',
@@ -29,6 +29,9 @@ import { staffs, Staff } from '../../staff/staff.model';
   styleUrls: ['./acsx290.component.scss']
 })
 export class ACSx290Component extends RegistryFormComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  //#region FormGroup and FormDirective
+
   formDetail: FormDetail;
   formGroupA: FormGroup;
   formGroupB: FormGroup;
@@ -78,209 +81,32 @@ export class ACSx290Component extends RegistryFormComponent implements OnInit, A
   @ViewChild('formDirectiveR', { static: true }) formDirectiveR: FormGroupDirective;
   @ViewChild('formDirectiveS', { static: true }) formDirectiveS: FormGroupDirective;
 
-  gap = '20px';
-  public mode = 'new'; // new, edit
-  private formId: string;
-  toc = tableOfContent;
-  private subscriptions: Subscription[] = [];
+  //#endregion
 
-  result: ACSx290Model;
-  flatResult: object;
+  //#region form data
 
-  staff = staffs;
+  // tslint:disable: variable-name
+  H_cathResults = acsx290Data.H_cathResults;
+  J_cabg = acsx290Data.J_cabg;
+  H_vad = acsx290Data.H_vad;
+  M2_priorAoInt = acsx290Data.M2_priorAoInt;
+  M2_device = acsx290Data.M2_device;
+  // tslint:enable: variable-name
 
   cvt: Staff[];
   anesth: Staff[];
   rn: Staff[];
   ctt: Staff[];
 
-  // tslint:disable: variable-name
-  H_cathResults = [
-    ['Left Main', 'PctStenLMain', 'GrftStenLMain', 'StntStenLMain', 'FFRLMain', 'IFRLMain'],
-    ['Proximal LAD', 'PctStenProxLAD', 'GrftStenProxLAD', 'StntStenProxLAD', 'FFRProxLAD', 'IFRProxLAD'],
-    ['Mid LAD', 'PctStenMidLAD', 'GrftStenMidLAD', 'StntStenMidLAD', 'FFRMidLAD', 'IFRMidLAD'],
-    ['Distal LAD', 'PctStenDistLAD', 'GrftStenDistLAD', 'StntStenDistLAD', 'FFRDistLAD', 'IFRDistLAD'],
-    ['Diagonal 1', 'PctStenDiag1', 'GrftStenDiag1', 'StntStenDiag1', 'FFRDiag1', 'IFRDiag1'],
-    ['Diagonal 2', 'PctStenDiag2', 'GrftStenDiag2', 'StntStenDiag2', 'FFRDiag2', 'IFRDiag2'],
-    ['Diagonal 3', 'PctStenDiag3', 'GrftStenDiag3', 'StntStenDiag3', 'FFRDiag3', 'IFRDiag3'],
-    ['Circumflex', 'PctStenCircflx', 'GrftStenCircflx', 'StntStenCircflx', 'FFRCircflx', 'IFRCircflx'],
-    ['Obtuse Marginal 1', 'PctStenOM1', 'GrftStenOM1', 'StntStenOM1', 'FFROM1', 'IFROM1'],
-    ['Obtuse Marginal 2', 'PctStenOM2', 'GrftStenOM2', 'StntStenOM2', 'FFROM2', 'IFROM2'],
-    ['Obtuse Marginal 3', 'PctStenOM3', 'GrftStenOM3', 'StntStenOM3', 'FFROM3', 'IFROM3'],
-    ['Ramus', 'PctStenRamus', 'GrftStenRamus', 'StntStenRamus', 'FFRRamus', 'IFRRamus'],
-    ['RCA', 'PctStenRCA', 'GrftStenRCA', 'StntStenRCA', 'FFRRCA', 'IFRRCA'],
-    ['Acute Marginal (AM)', 'PctStenAM', 'GrftStenAM', 'StntStenAM', 'FFRAM', 'IFRAM'],
-    ['Posterior Descending (PDA)', 'PctStenPDA', 'GrftStenPDA', 'StntStenPDA', 'FFRPDA', 'IFRPDA'],
-    ['Posterolateral (PLB)', 'PctStenPLB', 'GrftStenPLB', 'StntStenPLB', 'FFRPLB', 'IFRPLB']
-  ];
-  J_cabg = [
-    [
-      'CABG NUMBER 1',
-      'CAB01',
-      'CABDistSite01',
-      'CABProximalSite01',
-      'CABConduit01',
-      'CABDistPos01',
-      'CABEndArt01',
-      'CABVeinPatAng01'
-    ],
-    [
-      'CABG NUMBER 2',
-      'CAB02',
-      'CABDistSite02',
-      'CABProximalSite02',
-      'CABConduit02',
-      'CABDistPos02',
-      'CABEndArt02',
-      'CABVeinPatAng02'
-    ],
-    [
-      'CABG NUMBER 3',
-      'CAB03',
-      'CABDistSite03',
-      'CABProximalSite03',
-      'CABConduit03',
-      'CABDistPos03',
-      'CABEndArt03',
-      'CABVeinPatAng03'
-    ],
-    [
-      'CABG NUMBER 4',
-      'CAB04',
-      'CABDistSite04',
-      'CABProximalSite04',
-      'CABConduit04',
-      'CABDistPos04',
-      'CABEndArt04',
-      'CABVeinPatAng04'
-    ],
-    [
-      'CABG NUMBER 5',
-      'CAB05',
-      'CABDistSite05',
-      'CABProximalSite05',
-      'CABConduit05',
-      'CABDistPos05',
-      'CABEndArt05',
-      'CABVeinPatAng05'
-    ],
-    [
-      'CABG NUMBER 6',
-      'CAB06',
-      'CABDistSite06',
-      'CABProximalSite06',
-      'CABConduit06',
-      'CABDistPos06',
-      'CABEndArt06',
-      'CABVeinPatAng06'
-    ],
-    [
-      'CABG NUMBER 7',
-      'CAB07',
-      'CABDistSite07',
-      'CABProximalSite07',
-      'CABConduit07',
-      'CABDistPos07',
-      'CABEndArt07',
-      'CABVeinPatAng07'
-    ],
-    [
-      'CABG NUMBER 8',
-      'CAB08',
-      'CABDistSite08',
-      'CABProximalSite08',
-      'CABConduit08',
-      'CABDistPos08',
-      'CABEndArt08',
-      'CABVeinPatAng08'
-    ],
-    [
-      'CABG NUMBER 9',
-      'CAB09',
-      'CABDistSite09',
-      'CABProximalSite09',
-      'CABConduit09',
-      'CABDistPos09',
-      'CABEndArt09',
-      'CABVeinPatAng09'
-    ],
-    [
-      'CABG NUMBER 10',
-      'CAB10',
-      'CABDistSite10',
-      'CABProximalSite10',
-      'CABConduit10',
-      'CABDistPos10',
-      'CABEndArt10',
-      'CABVeinPatAng10'
-    ]
-  ];
-  H_vad = [
-    [
-      'Initial implant',
-      'VImp',
-      'VADImpTmg',
-      'VImpDt',
-      'VADInd',
-      'VImpTy',
-      'VProdTy',
-      'VImpUDI',
-      'VExp',
-      'VExpRsn',
-      'VExpDt'
-    ],
-    [
-      '2nd device implanted',
-      'VImp2',
-      'VADImpTmg2',
-      'VImpDt2',
-      'VADInd2',
-      'VImpTy2',
-      'VProdTy2',
-      'VImpUDI2',
-      'VExp2',
-      'VExpRsn2',
-      'VExpDt2'
-    ],
-    [
-      '3rd device implanted',
-      'VImp3',
-      'VADImpTmg3',
-      'VImpDt3',
-      'VADInd3',
-      'VImpTy3',
-      'VProdTy3',
-      'VImpUDI3',
-      'VExp3',
-      'VExpRsn3',
-      'VExpDt3'
-    ]
-  ];
-  M2_priorAoInt = [
-    ['Root', 'PriorRepRoot', 'PriorRepTyRoot', 'PriorFailRoot', 'PriorProgRoot'],
-    ['Ascending', 'PriorRepAsc', 'PriorRepTyAsc', 'PriorFailAsc', 'PriorProgAsc'],
-    ['Arch', 'PriorRepArch', 'PriorRepTyArch', 'PriorFailArch', 'PriorProgArch'],
-    ['Descending', 'PriorRepDesc', 'PriorRepTyDesc', 'PriorFailDesc', 'PriorProgDesc'],
-    ['Suprarenal abdominal', 'PriorRepSupraAb', 'PriorRepTySupraAb', 'PriorFailSupraAb', 'PriorProgSupraAb'],
-    ['Infrarenal abdominal', 'PriorRepInfraAb', 'PriorRepTyInfraAb', 'PriorFailInfraAb', 'PriorProgInfraAb']
-  ];
-  M2_device = [
-    ['ADevLoc02', 'ADevDelMeth02', 'ADevOut02', 'ADevModel02', 'ADevUDI02'],
-    ['ADevLoc03', 'ADevDelMeth03', 'ADevOut03', 'ADevModel03', 'ADevUDI03'],
-    ['ADevLoc04', 'ADevDelMeth04', 'ADevOut04', 'ADevModel04', 'ADevUDI04'],
-    ['ADevLoc05', 'ADevDelMeth05', 'ADevOut05', 'ADevModel05', 'ADevUDI05'],
-    ['ADevLoc06', 'ADevDelMeth06', 'ADevOut06', 'ADevModel06', 'ADevUDI06'],
-    ['ADevLoc07', 'ADevDelMeth07', 'ADevOut07', 'ADevModel07', 'ADevUDI07'],
-    ['ADevLoc08', 'ADevDelMeth08', 'ADevOut08', 'ADevModel08', 'ADevUDI08'],
-    ['ADevLoc09', 'ADevDelMeth09', 'ADevOut09', 'ADevModel09', 'ADevUDI09'],
-    ['ADevLoc10', 'ADevDelMeth10', 'ADevOut10', 'ADevModel10', 'ADevUDI10'],
-    ['ADevLoc11', 'ADevDelMeth11', 'ADevOut11', 'ADevModel11', 'ADevUDI11'],
-    ['ADevLoc12', 'ADevDelMeth12', 'ADevOut12', 'ADevModel12', 'ADevUDI12'],
-    ['ADevLoc13', 'ADevDelMeth13', 'ADevOut13', 'ADevModel13', 'ADevUDI13'],
-    ['ADevLoc14', 'ADevDelMeth14', 'ADevOut14', 'ADevModel14', 'ADevUDI14'],
-    ['ADevLoc15', 'ADevDelMeth15', 'ADevOut15', 'ADevModel15', 'ADevUDI15']
-  ];
-  // tslint:enable: variable-name
+  toc = tableOfContent;
+
+  //#endregion
+
+  gap = '20px';
+  public mode = 'new'; // new, edit
+  private formId: string;
+  private subscriptions: Subscription[] = [];
+  result: ACSx290Model;
 
   constructor(
     protected dialogService: DialogService,
--- src/app/feature/staff/staff.model.ts
+++ src/app/feature/staff/staff.model.ts
@@ -19,36 +19,3 @@ export interface Staff {
   modifiedAt?: any;
   modifiedBy?: string;
 }
-
-export const staffs: Staff[] = [
-  {
-    staffId: '001',
-    title: 'Mr',
-    firstName: 'Art',
-    lastName: 'AA',
-    position: 'CVT',
-    primaryHospId: 'BHT',
-    registries: ['ACSx290'],
-    status: 'Active'
-  },
-  {
-    staffId: '003',
-    title: 'Ms',
-    firstName: 'Kwan',
-    lastName: 'BB',
-    position: 'Nurse',
-    primaryHospId: 'BHT',
-    registries: ['ACSx290'],
-    status: 'Active'
-  },
-  {
-    staffId: '003',
-    title: 'Mrs',
-    firstName: 'Auh',
-    lastName: 'CC',
-    position: 'Cardiologist',
-    primaryHospId: 'BHT',
-    registries: ['ACSx290'],
-    status: 'Active'
-  }
-];
