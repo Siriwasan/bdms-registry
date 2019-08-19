b1dadf396542cf8f9b2fccc4fc5b0095ba29b800
Siriwasan <siriwasan@gmail.com>
2019-08-19 20:37:31 +0700

    add firebase Dev deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appKey: 'SNlQeCBW5FbTMvGMNquBwFaaUw0n7nKI',
   firebase: {
     apiKey: 'AIzaSyBOpwqWngR-DBwOBJsrxlMsCPmZAwYGcQs',
