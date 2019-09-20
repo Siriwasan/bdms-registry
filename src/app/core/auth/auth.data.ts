8427102611964a9c7ff086d03ce8b86e19f5b0b5
Siriwasan <siriwasan@gmail.com>
2019-09-20 14:04:05 +0700

    add Hospital data

--- src/app/core/auth/auth.data.ts
+++ src/app/core/auth/auth.data.ts
@@ -17,7 +17,16 @@ export interface Hospital {
 
 export const hospitals: Hospital[] = [
   { group: 'Group1', id: 'BHT', name: 'Bangkok Heart Hospital' },
-  { group: 'Group4', id: 'BCM', name: 'Bangkok Chiang Mai Hospital' }
+  { group: 'Group1', id: 'BSN', name: 'Bangkok Hospital Sanamchan' },
+  { group: 'Group2', id: 'SVH', name: 'Samitivej Sukhumvit Hospital' },
+  { group: 'Group3', id: 'BPH', name: 'Bangkok Hospital Pattaya' },
+  { group: 'Group4', id: 'BUD', name: 'Bangkok Hospital Udon' },
+  { group: 'Group4', id: 'BKH', name: 'Bangkok Hospital Ratchasima' },
+  { group: 'Group4', id: 'BKN', name: 'Bangkok Hospital Khon Kaen' },
+  { group: 'Group4', id: 'BCM', name: 'Bangkok Hospital Chiang Mai' },
+  { group: 'Group5', id: 'PT2', name: 'Phyathai 2 Hospital' },
+  { group: 'Group6', id: 'BHH', name: 'Bangkok Hospital Hatyai' },
+  { group: 'Group6', id: 'BPK', name: 'Bangkok Hospital Phuket' }
 ];
 
 export const roles = ['Director', 'Administrator', 'Editor', 'Staff'];
